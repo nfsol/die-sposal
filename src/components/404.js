4872d5fb402616a38e3e82301f75a05cfec590cc
Tim P <tbpeddle@gmail.com>
2022-02-13 20:52:55 -0330

    basic 404 page in place

---
+++ src/components/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Rabble from "./Rabble";
+
+const NotFound = () => {
+  return (
+    <div className="container">
+
+    <div className="notFound">
+      {" "}
+      <h1>Whoops, looks like you took a wrong turn.</h1>
+      <Rabble />{" "}
+      <p>
+        Ea sunt ut magna ex incididunt. Id anim consectetur nostrud cupidatat
+        incididunt laboris nulla anim ut. Lorem velit adipisicing magna ad id
+        officia reprehenderit.
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import './index.css';
-import App from './App';
-import Header from './components/Header'
-import Reviews from './components/Reviews'
-import Services from './components/Services'
-import Cleanse from './components/Cleanse'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Header from "./components/Header";
+import Reviews from "./components/Reviews";
+import Services from "./components/Services";
+import Cleanse from "./components/Cleanse";
+import NotFound from "./components/404";
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-      <Route
-        index
-        element={<Header />
-        }
-      />
-      <Route path="reviews" element={<Reviews />} />
-      <Route path="services" element={<Services/>}/>
+        <Route index element={<Header />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="services" element={<Services />} />
         <Route path="cleanse" element={<Cleanse />} />
-        </Route>
-       
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
